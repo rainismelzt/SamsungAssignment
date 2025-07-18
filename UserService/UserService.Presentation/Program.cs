14938b376b8ddfa6a216f4a0123162addb1cd557
Leow Zheng Tao <rainismelzt98@gmail.com>
2025-07-18 10:28:59 +0800

    - updated pipeline

--- UserService/UserService.Presentation/Program.cs
+++ UserService/UserService.Presentation/Program.cs
@@ -1,55 +1,9 @@
-using CommonLibrary.DependencyProvider;
-using Microsoft.OpenApi.Models;
-using UserService.Core.EntityMapper;
-using UserService.Core.Interface;
-using UserService.Database;
-using UserService.Infrastructure.Repository;
-using UserService.Infrastructure.Service;
-using UserService.Presentation.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddConfiguredDbContext<UserDbContext>(builder.Configuration);
-builder.Services.AddScoped<IUserDataRepository, UserDataRepository>();
-builder.Services.AddScoped<IUserDataService, UserDataService>();
-builder.Services.AddAutoMapper(cfg =>
-{
-    cfg.AddProfile<MappingProfile>();
-});
-builder.Services.AddControllers();
-builder.Services.AddAngularCorsPolicy(builder.Configuration);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
-});
+// Add services and configuration from Startup
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
-    });
-}
-
-app.UseCors("AngularCors");
-
-app.UseMiddleware<HtmlSanitizerMiddleware>();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-// Only run the app if not being called by the Swashbuckle CLI
-if (!app.Environment.IsEnvironment("swagger"))
-{
-    app.Run();
-}
+app.Run();
---
+++ UserService/UserService.Presentation/Startup.cs
@@ -0,0 +1,63 @@
+﻿using CommonLibrary.DependencyProvider;
+using Microsoft.OpenApi.Models;
+using UserService.Core.EntityMapper;
+using UserService.Core.Interface;
+using UserService.Database;
+using UserService.Infrastructure.Repository;
+using UserService.Infrastructure.Service;
+using UserService.Presentation.Middleware;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    // Register services
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddConfiguredDbContext<UserDbContext>(Configuration);
+
+        services.AddScoped<IUserDataRepository, UserDataRepository>();
+        services.AddScoped<IUserDataService, UserDataService>();
+
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+
+        services.AddControllers();
+        services.AddAngularCorsPolicy(Configuration);
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+        });
+    }
+
+    // Configure middleware pipeline
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+            });
+        }
+
+        app.UseCors("AngularCors");
+
+        app.UseMiddleware<HtmlSanitizerMiddleware>();
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
+}
